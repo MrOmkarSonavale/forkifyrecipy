ac70a68e3280ae02330f441b561e91fef350ae4b
MrOmkarSonavale <mr.sonavaleomkar@gmail.com>
2024-06-07 22:51:38 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6fcc8828-7830-4256-9b70-07c050e1be1b';
+export const MODAL_CLOSE_SEC = 2.5;
